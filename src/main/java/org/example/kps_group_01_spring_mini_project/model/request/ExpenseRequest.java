eb6ce44b045dfd08b7ae1e6e8c7130c686ea1bf9
lynita <nitaly0122@gmail.com>
2024-04-17 14:33:53 +0700

    new repo

--- src/main/java/org/example/kps_group_01_spring_mini_project/model/request/ExpenseRequest.java
+++ src/main/java/org/example/kps_group_01_spring_mini_project/model/request/ExpenseRequest.java
@@ -1,5 +1,17 @@
 package org.example.kps_group_01_spring_mini_project.model.request;
 
-public class ExpenseRequest {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExpenseRequest {
+    private Integer amount;
+    private String description;
+    private LocalDateTime date;
+    private Integer category_id;
 }
--- src/main/java/org/example/kps_group_01_spring_mini_project/repository/ExpenseRepository.java
+++ src/main/java/org/example/kps_group_01_spring_mini_project/repository/ExpenseRepository.java
@@ -1,9 +1,6 @@
 package org.example.kps_group_01_spring_mini_project.repository;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 import org.example.kps_group_01_spring_mini_project.model.Expense;
 import org.example.kps_group_01_spring_mini_project.model.request.ExpenseRequest;
 
@@ -12,14 +9,15 @@ public interface ExpenseRepository {
 
     @Select("SELECT * FROM expenses")
     Expense getAllExpenses(Integer offset, Integer limit, String sortBy, String orderBy);
-    @Select("INSERT INTO expenses RETURNING *")
-    Expense createExpense(ExpenseRequest expenseRequest);
 
-    @Select("SELECT * FROM expenses WHERE id = #{id}")
+    @Select("SELECT * FROM expenses WHERE expense_id = #{id}::uuid")
     Expense getExpenseById(Integer id);
 
-    @Update("UPDATE expenses SET description = #{description}, amount = #{amount} WHERE id = #{id}")
-    Expense updateExpense(Integer id, ExpenseRequest expenseRequest);
+    @Select("INSERT INTO expense (amount,description,date,category_id) VALUES ( #{expense.category_id}::uuid) RETURNING *")
+    Expense createExpense(@Param("expense")ExpenseRequest expenseRequest);
+
+    @Select("UPDATE expense SET amount = #{amount}, description = #{expense.description}, date = #{expense.date }, category_id = #{expense.category_id }::uuid")
+    Expense updateExpense(Integer id, @Param("expense") ExpenseRequest expenseRequest);
 
     @Delete("DELETE FROM expenses WHERE id = #{id}")
     Expense deleteExpense(Integer id);
